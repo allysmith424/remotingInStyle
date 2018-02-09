5d792719a387c57e8c871d03df2213fe6aaab5fb
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-02-09 09:12:17 -0600

    testing

--- assets/javascript/page2Logic.js
+++ assets/javascript/page2Logic.js
@@ -2,6 +2,8 @@
 
 // populate DOM with day icons, high temp, low temp
 
+var hotelCounter = 0;
+
 var currentCity = "San Francsico";
 
 var latitude = "37.7749"
@@ -72,7 +74,66 @@ function findForecast(latitude, longitude) {
 
 };
 
+function loadHotels(latitude, longitude) {
+
+	var queryURL = "https://api.sandbox.amadeus.com/v1.2/hotels/search-circle?apikey=iD5zJSk96ckruurDP9FraQIVA5ROplcG&latitude=" + latitude + "&longitude=" + longitude + "&radius=40&check_in=2018-02-10&check_out=2018-02-11&number_of_results=5";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function(response) {
+
+		$("#main-content").append("<div class='pure-u-1-5'>&nbsp;</div>");
+
+		var hotelDiv = $("<div>");
+		hotelDiv.attr("id", "hotel-div").addClass("pure-u-3-5");
+
+		var img = $("<img>");
+		img.attr("src", "assets/images/hotel.png");
+		img.addClass("hotel-image");
+
+		var hotelInfo = $("<div>");
+		hotelInfo.addClass("hotel-info");
+
+		var hotelName = $("<h4>");
+		hotelName.text(response.results[hotelCounter].property_name);
+
+		var hotelAddress = $("<p>");
+		hotelAddress.addClass("hotel-address").append(response.results[hotelCounter].address.line1).append("<br>").append(response.results[hotelCounter].address.city).append("<br>").append(response.results[hotelCounter].address.postal_code);
+
+		var hotelWebsite = $("<p>");
+		var hotelLink = $("<a>");
+		var link = response.results[hotelCounter]._links.href;
+		console.log(link);
+		hotelLink.attr("href", link);
+
+		hotelLink.text("Website");
+		hotelWebsite.append(hotelLink);
+
+		hotelInfo.append(hotelName, hotelAddress, hotelWebsite);
+
+		var nextBtn = $("<button>");
+		nextBtn.addClass("next-btn").text("next");
+
+		hotelDiv.append(img, hotelInfo, nextBtn);
+
+		$("#main-content").append(hotelDiv);
+
+		$("#main-content").append("<div class='pure-u-1-5'>&nbsp;</div>");
+
+	});
+
+};
+
 $(document).ready(function() {
+
+	$(".hotel-options").on("click", function() {
+
+		$("#main-content").empty();
+
+		loadHotels(latitude, longitude);
+
+	});
 
 	findForecast(latitude, longitude);
 
